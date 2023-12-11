1d35e789e4ba33e1a2084762699dc9e3a44246dd
Sergey <karakoskinsergej@gmail.com>
2023-12-11 21:04:10 +0300

    create class MenuCard

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     //Timer
 
-    const deadline = '2023-12-10'; // создаем дедлайн
+    const deadline = '2024-01-01'; // создаем дедлайн
 
     function getTimeRemaining(endtime) {  // создаем функцию которая будет определять разницу между настоящим временем и дедлайном(endtime)
         const t = Date.parse(endtime) - Date.parse(new Date());
@@ -127,5 +127,64 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
     
+    // CLASS
+
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.transfer = 27; // можно взять из стороннего сервера с курсами на сегодняшний день
+            this.parent = document.querySelector(parentSelector);
+            this.changeToUAH();// вызов метода конвертации
+        }
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+        render() {
+            const element = document.createElement('div'); // создаем элемент и далее копируем в него верстку из HTML. Заменяем нужное аргументами
+            element.innerHTML = `
+                <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>
+            `;
+            this.parent.append(element); // размещение на странице
+        }
+    }
+
+    new MenuCard( // создаем карточки используя JS. После создания удаляем карточки из HTML -> теперь все карточки (включая вновь созданные) будут созданы из одного класса (шаблона)
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        '.menu .container'
+    ).render();
+
+    new MenuCard( // создаем карточки используя JS. После создания удаляем карточки из HTML -> теперь все карточки (включая вновь созданные) будут созданы из одного класса (шаблона)
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        20,
+        '.menu .container'
+    ).render(); 
+
+    new MenuCard( // создаем карточки используя JS. После создания удаляем карточки из HTML -> теперь все карточки (включая вновь созданные) будут созданы из одного класса (шаблона)
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        14,
+        '.menu .container'
+    ).render(); 
 
 });
