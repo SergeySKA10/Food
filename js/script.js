e3ba11c0a9a0505e31d2b7f5639407067b731aaa
Sergey <karakoskinsergej@gmail.com>
2023-12-12 20:26:56 +0300

    add Rest and default class for MenuCard

--- js/script.js
+++ js/script.js
@@ -126,16 +126,18 @@ window.addEventListener('DOMContentLoaded', () => {
             modalClose();
         }
     });
+
     
     // CLASS
 
     class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) { //используем Rest для добавления классов элементам
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.classes = classes;
             this.transfer = 27; // можно взять из стороннего сервера с курсами на сегодняшний день
             this.parent = document.querySelector(parentSelector);
             this.changeToUAH();// вызов метода конвертации
@@ -145,16 +147,20 @@ window.addEventListener('DOMContentLoaded', () => {
         }
         render() {
             const element = document.createElement('div'); // создаем элемент и далее копируем в него верстку из HTML. Заменяем нужное аргументами
+            if(this.classes.length === 0) {
+                this.element = 'menu__item';
+                element.classList.add(this.element); // если класс не указан, то будет значение по default
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
             element.innerHTML = `
-                <div class="menu__item">
-                    <img src=${this.src} alt=${this.alt}>
-                    <h3 class="menu__item-subtitle">${this.title}</h3>
-                    <div class="menu__item-descr">${this.descr}</div>
-                    <div class="menu__item-price">
-                        <div class="menu__item-cost">Цена:</div>
-                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                    </div>
-                </div>
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div> 
             `;
             this.parent.append(element); // размещение на странице
         }
@@ -175,7 +181,8 @@ window.addEventListener('DOMContentLoaded', () => {
         'Меню “Премиум”',
         'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
         20,
-        '.menu .container'
+        '.menu .container',
+        'menu__item'
     ).render(); 
 
     new MenuCard( // создаем карточки используя JS. После создания удаляем карточки из HTML -> теперь все карточки (включая вновь созданные) будут созданы из одного класса (шаблона)
@@ -184,7 +191,8 @@ window.addEventListener('DOMContentLoaded', () => {
         'Меню "Постное"',
         'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
         14,
-        '.menu .container'
+        '.menu .container',
+        'menu__item'
     ).render(); 
 
 });
